9a34ff6c907e12ade50e45f14216d4e03c581fa4
Agustin Gambina <agusgambina@gmail.com>
2018-07-04 16:32:38 -0300

    fix findById

--- app/repositories/GameRepository.scala
+++ app/repositories/GameRepository.scala
@@ -1,7 +1,7 @@
 package repositories
 
 import models.Game
-import org.mongodb.scala.bson.ObjectId
+import org.mongodb.scala.model.Filters._
 import org.mongodb.scala._
 import play.api.libs.json.Json
 
@@ -27,16 +27,15 @@ class GameRepository(collection: MongoCollection[Game])(implicit ec: ExecutionCo
     }, 10 seconds)
   }
 
-  def find(gameId: Int): Either[String, Game] = ???
-//  {
-//    Await.result(collection
-//      .find(Document("id" -> new ObjectId(gameId)))
-//      .first
-//      .head
-//      .map(Option(_)) map {
-//      case Some(game: Game) => Right(game)
-//      case _ => Left("An error has occurred")
-//    }, 10 seconds)
-//  }
+  def find(gameId: Int): Either[String, Game] = {
+    Await.result(collection
+      .find(equal("id", gameId))
+      .first
+      .head
+      .map(Option(_)) map {
+      case Some(game: Game) => Right(game)
+      case _ => Left("An error has occurred")
+    }, 10 seconds)
+  }
 
 }
